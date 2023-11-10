c238fb16a129eec351dfb1dfa57b39fffd7a1058
William Crupi <crupib@gmail.com>
2023-11-10 12:27:18 -0500

    groovy

---
+++ ActivityCounter.groovy
@@ -0,0 +1,6 @@
+def completedActivities = 0
+completedActivities = completedActivities + 5
+println "You have completed ${completedActivities=completedActivities+1} activites today."
+def totalActivities = 10
+totalActivities -= completedActivities
+println "You now have $totalActivities activites left to do."
